7e96863521fe88434d8e70a41ad85243b4c7d737
wupojung <wupojung@gmail.com>
2020-03-13 20:27:21 +0800

    加入專案檔案。

---
+++ Test20200313/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Test20200313
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int hp = 50;
+            while (true)
+            {
+                //check is game over 
+                if (hp <= 0)
+                {
+                    Console.WriteLine("GG");
+                    break;
+                }
+                //input attack
+                string input = Console.ReadLine();
+                int attack = int.Parse(input);
+                hp = hp - attack;
+
+                //display
+                Console.WriteLine("HP =" + hp);
+            }
+            Console.ReadKey();
+        }
+    }
+}
